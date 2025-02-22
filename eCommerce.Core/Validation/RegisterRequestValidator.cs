5380f108ea177f3403d412e1994238ca90c23a91
ravisingh1994 <ravipratapsingh2345@gmail.com>
2025-02-22 13:38:13 +0530

    #Validation Msg Changes

--- eCommerce.Core/Validation/RegisterRequestValidator.cs
+++ eCommerce.Core/Validation/RegisterRequestValidator.cs
@@ -8,8 +8,8 @@ public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
     public RegisterRequestValidator()
     {
         RuleFor(item => item.Email).NotEmpty().WithMessage("Email can't be empty")
-            .EmailAddress().WithMessage("Etner the write Email");
-        RuleFor(item => item.PersonName).NotEmpty().WithMessage("Person can't be null")
+            .EmailAddress().WithMessage("Enter the write Email");
+        RuleFor(item => item.PersonName).NotEmpty().WithMessage("Person can't be Blank")
             .MaximumLength(50);
         RuleFor(item => item.Password).NotEmpty().WithMessage("Password can't be Empty")
            .MaximumLength(50);
